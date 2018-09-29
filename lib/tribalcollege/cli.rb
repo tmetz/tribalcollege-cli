e960b07f92f26599d0b19afe29ca599df99c1343
Tammy Metz <tammy@tammymetz.com>
2018-09-29 23:03:43 +0000

    first commit

---
+++ lib/tribalcollege/cli.rb
@@ -0,0 +1,7 @@
+require "tribalcollege/cli/version"
+
+module Tribalcollege
+  module Cli
+    # Your code goes here...
+  end
+end
---
+++ lib/tribalcollege/cli/version.rb
@@ -0,0 +1,5 @@
+module Tribalcollege
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
