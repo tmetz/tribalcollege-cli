94e84917aff32d6f467f7784fb6f4df300782c33
Tammy Metz <tammy@tammymetz.com>
2018-10-10 22:00:08 +0000

    Stubbing out structure

--- lib/tribalcollege/cli.rb
+++ lib/tribalcollege/cli.rb
@@ -1,6 +1,39 @@
 class Tribalcollege::CLI
   def call
-    puts "Thank you for using Tribal College Listings!"
+    command = ""
+    while command != '4'
+      puts("Welcome to the tribal college directory!")
+      puts("For information/help, type 1")
+      puts("To list all tribal colleges, type 2")
+      puts("To list all tribal colleges in a specific state, type 3")
+      puts("To quit, type 4")
+      puts("What would you like to do?")
+      command = gets.strip
+      case command
+      when "1"
+        list_help
+      when "2"
+        list_colleges
+      when "3"
+        select_state
+      when "4"
+        puts "Thank you for using the tribal college directory.  Have a great day!"
+      else
+        puts "That is not a valid option"
+      end
+    end
+  end
+  
+  def list_help
+    
+  end
+  
+  def list_colleges
+    
+  end
+  
+  def select_state
+    
   end
   
 end
